9275e4bca563a7e83962b9fb9a3111695ef0a373
Alexander Lifanov <lifanov.a.v@gmail.com>
2017-10-06 18:01:04 +0300

    add package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mikasa',
+      version='0.1',
+      description='Event driven backtesting tool',
+      url='http://github.com/alifanov/mikasa_ed',
+      author='Alexander Lifanov',
+      author_email='jetbootsmaker@gmail.com',
+      license='MIT',
+      packages=['mikasa'],
+      zip_safe=False)
